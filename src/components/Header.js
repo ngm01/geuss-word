b832d4a649479b48fba15b8eb6c1eb2e31e590c2
ngm01 <nathaniel.g.moore@gmail.com>
2023-01-20 21:14:15 -0500

    started work on title flip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
 import "../styles/Header.css";
+import LetterSquare from "./LetterSquare";
 
 function Header() {
+
+    const [titleA, setTitleA] = useState([
+        {letter: 'G', inPosition: false, inWord: false},
+        {letter: 'U', inPosition: false, inWord: false},
+        {letter: 'E', inPosition: false, inWord: false},
+        {letter: 'S', inPosition: false, inWord: false},
+        {letter: 'S', inPosition: false, inWord: false},
+    ]);
+    const [titleB, setTitleB] = useState([
+        {letter: 'W', inPosition: false, inWord: false},
+        {letter: 'O', inPosition: false, inWord: false},
+        {letter: 'R', inPosition: false, inWord: false},
+        {letter: 'K', inPosition: false, inWord: false},
+    ]);
+
+    window.setTimeout(function(){
+        setTitleA(titleA.map(letter => {return {...letter, inPosition: true, inWord: true}}))
+    }, 1500)
+    window.setTimeout(function(){
+        setTitleB(titleB.map(letter => {
+            if(letter.letter === 'K') {
+                return {...letter, inPosition: false, inWord: true}
+            } else {
+                return {...letter, inPosition: true, inWord: true}
+            }
+        }))
+    }, 2500)
+
     return ( 
         <div className="header">
-            <h1><del>Guesswork</del> Guessword</h1>
+            <div className="title">
+            {titleA.map((letter, i) =>
+                <LetterSquare rotate={letter.inWord} key={i} speed={i} letter={letter} />
+            )}
+            </div>
+            <div className="title">
+            {titleB.map((letter, i) =>  
+                <LetterSquare rotate={letter.inWord} key={i} speed={i} letter={letter} />
+            )}
+            </div>
             <p><em>TODO:</em> 'K' flips to become 'D'</p>
         </div>
      );
