7f1015f4166d59c1c966eda38d73e91ddb5973f7
ngm01 <nathaniel.g.moore@gmail.com>
2023-04-22 20:38:50 -0400

    fixed letter highlighting issue, modified stats popup styling

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -7,12 +7,16 @@ function Stats(props) {
     }
 
     return ( 
-        <div className={`stats ${props.show ? 'show' : 'hide'}`}>
+        <div className={`stats ${props.show ? 'showStats' : 'hideStats'}`}>
             <div className="statsClose" onClick={closeStats}>
                 <div>❌</div>
             </div>
             <div className="statsBody">
                 <p>Gameplay statistics coming soon!</p>
+            </div>
+            <div className="finePrint">
+                    <p >Made by <a target="_blank" rel="noreferrer" href="https://ngm01.com">Nathaniel Moore</a></p>
+                    <p>Last updated April 22, 2023</p>
             </div>
         </div>
      );
