730daa1a71ebe2700266997ec8e7c672f8c243e4
ngm01 <nathaniel.g.moore@gmail.com>
2023-04-30 17:28:52 -0400

    simplified guess parsing algo

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ function App() {
   useEffect(() => {
     fetch('https://7hf5905yka.execute-api.us-east-2.amazonaws.com/default/guessword-getter-v1')
       .then(res => res.json()).then(data => {
-        setTodaysWord(data.Items[0].word.split(''));
+        //setTodaysWord(data.Items[0].word.split(''));
+        setTodaysWord('BERRY'.split(''))
         const storageGuesses = localStorage.getItem('guesses');
         const storageDate = localStorage.getItem('date');
         const storageKeys = localStorage.getItem('keys');
@@ -233,23 +234,15 @@ function App() {
         let currentGuess = draft[progress];
         for(let i = 0; i < currentGuess.length; i++) {
           let letterToMatch = currentGuess[i];
-          const indexOfMatch = letterList.findIndex(letter => letter === letterToMatch.letter);
-          if(indexOfMatch !== -1 && letterList.includes(letterToMatch.letter)) {
-            if(indexOfMatch === i) {
+          const letterIndices = utils.getAllLetterIndices(letterList, letterToMatch.letter)
+          if(letterIndices.length > 0) {
+            if(letterIndices.includes(i)) {
               currentGuess[i].inWord = true;
               currentGuess[i].inPosition = true;
-              letterList.splice(letterList.indexOf(letterToMatch.letter), 1, null);
+              letterList.splice(i, 1, null);
             } else {
-              // ensure that the letter being evaluated is not used elsewhere in the guess.
-              // Trying to avoid a 'false positive' scenario, e.g. if 
-              // todaysWord = 'HALVE' and currentGuess = 'VALVE',
-              // 'V' at currentGuess[0] should not be marked as inWord, and we should continue iterating
-              // over currentGuess until we reach the 'V' at currentGuess[3]
-              const letterUseInGuess = utils.getAllLetterIndices(utils.getWordFromArray(currentGuess).split(''), letterToMatch.letter)
-              if(!letterUseInGuess.includes(indexOfMatch)) {
-                currentGuess[i].inWord = true;
-                letterList.splice(letterList.indexOf(letterToMatch.letter), 1, null);
-              }
+              currentGuess[i].inWord = true;
+              letterList.splice(i, 1, null);
             }
           }
         }
